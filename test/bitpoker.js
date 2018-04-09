8b8fd9a54e21cd9f2e1ec8b206033aaf647a7ef3
simon <me@example.org>
2018-04-09 21:57:02 +0800

    fix

--- test/bitpoker.js
+++ test/bitpoker.js
@@ -2,37 +2,40 @@ const BitPoker = artifacts.require("../contracts/BitPoker.sol");
 
 contract('BitPoker', async(accounts) => {
     it("test deposit ok", async () => {
-        let instance = await BitPoker.new();
+        let instance = await BitPoker.new({'from': accounts[0]});
 
         let v1 = 1130023456789012098, v2 = 1430023456789000000, userId = 92400;
 
-        await instance.sendTransaction({'value': v1, 'data': '092400'});
+        let r1 = await instance.sendTransaction({'value': v1, 'from': accounts[0]});
+        assert.equal(r1.logs[0].args.src, accounts[0]);
+
+        let r2 = await instance.confirmDeposit([accounts[0]], [userId], {'from': accounts[0]});
+        assert.equal(r2.logs[0].args.userId, userId);
+        assert.equal(r2.logs[0].args.balance, v1);
 
         let b1 = await instance.balanceOf.call(userId);
         assert.equal(b1.toNumber(), v1);
 
-        let result = await instance.sendTransaction({'value': v2, 'data': '092400'});
-
-        assert.equal(result.logs[0].args.userId.toNumber(), userId);
-        assert.equal(result.logs[0].args.amount.toNumber(), v2);
-
-        assert.ok(result.logs[0].args.balance, v1 + v2);
+        await instance.sendTransaction({'value': v2, 'from': accounts[0]});
+        await instance.confirmDeposit([accounts[0]], [userId], {'from': accounts[0]});
 
         let b2 = await instance.balanceOf.call(userId);
-        assert.equal(b2, v1 + v2);
+        assert.equal(b2.toNumber(), v1 + v2);
     });
 
     it("test withdraw ok", async () => {
         let instance = await BitPoker.new({'from': accounts[0]});
 
-        let v = 1130023456789012098;
+        let v = 1130023456789012098, userId = 123;
 
-        await instance.sendTransaction({'value': v, 'data': '123'});
+        await instance.sendTransaction({'value': v, 'from': accounts[0]});
 
-        await instance.transferTo(123, accounts[1], v, {'from': accounts[0]});
+        await instance.confirmDeposit([accounts[0]], [userId], {'from': accounts[0]});
+
+        await instance.transferTo(userId, accounts[1], v, {'from': accounts[0]});
 
         try {
-            await instance.transferTo(123, accounts[1], 1, {'from': accounts[0]});
+            await instance.transferTo(userId, accounts[1], 1, {'from': accounts[0]});
             assert.ok(false, "no result should be returned.");
         } catch (err) {
             assert.ok(true, "exception should be raised.");
