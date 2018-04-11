4a7413be18dc7a93e045451e7abf63652dc04888
simon <me@example.org>
2018-04-11 21:57:28 +0800

    fix

--- contracts/BitPoker.sol
+++ contracts/BitPoker.sol
@@ -84,22 +84,4 @@ contract BitPoker is Owned {
     function close(address dest) public onlyOwner {
         selfdestruct(dest);
     }
-
-    // 从转账信息中抽取用户ID
-    function extractUserId(bytes value) private pure returns (uint32) {
-        uint32 ret = 0;
-        for (uint8 i = 0; i < 4 && i < value.length; i++) {
-            uint8 v1 = uint8(value[i] & 0x0F);
-            assert(v1 >= 0 && v1 <= 9);
-
-            uint8 v2 = uint8(value[i] & 0xF0) / 16;
-            assert(v2 >= 0 && v2 <= 9);
-
-            ret = ret * 100 + v2 * 10 + v1;
-        }
-
-        assert(ret > 0 && ret <= 99999999);
-
-        return ret;
-    }
 }
