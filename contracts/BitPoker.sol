b444408df0ee1f26ecf1511b1a947ad7fbb4e4d0
simon <me@example.org>
2018-05-04 20:46:04 +0800

    fix

--- contracts/BitPoker.sol
+++ contracts/BitPoker.sol
@@ -1,4 +1,48 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.21;
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
 
 contract Owned {
@@ -15,6 +59,8 @@ contract Owned {
 
 
 contract BitPoker is Owned {
+
+    using SafeMath for uint256;
 
     uint8 public version = 1;
 
