49b556ff8319fcda07eee03e555db711413b9da5
simon <me@example.org>
2018-05-09 08:53:13 +0800

    fix

--- contracts/BitPoker.sol
+++ contracts/BitPoker.sol
@@ -74,7 +74,7 @@ contract BitPoker is Owned {
 
     event Withdraw(uint32 userId, address dst, uint256 amount);
 
-    event Transfer(uint64 id, uint32 src, uint32 dst, uint256 amount);
+    event Transfer(uint64 id, uint32 src, uint32 dst, uint8 type, bool status);
 
     function () public payable {
         deposit();
@@ -123,7 +123,7 @@ contract BitPoker is Owned {
     }
 
     // 结算，即互相转账
-    function transfer(uint64[] ids, uint32[] src, uint32[] dst, uint256[] amount)
+    function transfer(uint64[] ids, uint32[] src, uint32[] dst, uint256[] amount, uint8 type)
             public onlyOwner {
         require(src.length > 0);
         require(src.length == dst.length && dst.length == amount.length && amount.length == ids.length);
@@ -133,7 +133,9 @@ contract BitPoker is Owned {
                 _balances[src[i]] = _balances[src[i]].sub(amount[i]);
                 _balances[dst[i]] = _balances[dst[i]].add(amount[i]);
 
-                emit Transfer(ids[i], src[i], dst[i], amount[i]);
+                emit Transfer(ids[i], src[i], dst[i], type, true);
+            } else {
+                emit Transfer(ids[i], src[i], dst[i], type, false);
             }
         }
     }
